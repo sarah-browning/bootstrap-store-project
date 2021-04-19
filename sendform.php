5c4afb87c57c85e587c55205a04fc614e99a5694
sarah-browning <browning.sarah.a@gmail.com>
2021-04-19 00:35:49 -0300

    Completed for turn-in.  Modal not working as cards are not responsive.

--- sendform.php
+++ sendform.php
@@ -1,36 +1,26 @@
 <?php
-if(isset( $_POST['name']))
-$name = $_POST['name'];
-if(isset( $_POST['email']))
-$email = $_POST['email'];
-if(isset( $_POST['message']))
-$message = $_POST['message'];
-if(isset( $_POST['subject']))
-$subject = $_POST['subject'];
-if ($name === ''){
-echo "Name cannot be empty.";
-die();
-}
-if ($email === ''){
-echo "Email cannot be empty.";
-die();
-} else {
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Email format invalid.";
-die();
-}
-}
-if ($subject === ''){
-echo "Subject cannot be empty.";
-die();
-}
-if ($message === ''){
-echo "Message cannot be empty.";
-die();
-}
-$content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "browning.sarah.a@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent!";
+    // retrieve all 6 values from the server
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $subject = $_POST['subject'];
+
+    // create variable for error codes
+    $errors = 0;
+
+    // error messages
+    if ($name == "") {$errors = 1; $errorMessage = "Name is required"; }
+    if ($email == "") {$errors = 2; $errorMessage = "Email address is required"; }
+    if ($message == "") {$errors = 3; $errorMessage = "Message is required"; }
+    if ($subject == "") {$errors = 4; $errorMessage = "Subject line is required"; }
+
+    // if error code is not 0
+    if ($errors != 0) {
+        //return error message
+        echo "errors detected: $errorMessage";
+    } else {
+    //otherwise
+        //return OK
+        echo "OK";
+    }
 ?>
